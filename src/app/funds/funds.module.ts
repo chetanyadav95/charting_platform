2753eac4b012f7dae5f9cc5c782e62dc51820c98
Rahul Agrawal <agrawalrahul1094@gmail.com>
2020-11-22 20:03:56 +0530

    add initial modules for dashboard, funds, login, positions, watchlist

---
+++ src/app/funds/funds.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FundsRoutingModule } from './funds-routing.module';
+import { FundsComponent } from './funds.component';
+
+
+@NgModule({
+  declarations: [FundsComponent],
+  imports: [
+    CommonModule,
+    FundsRoutingModule
+  ]
+})
+export class FundsModule { }
