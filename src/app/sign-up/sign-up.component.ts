3912f10e4477d7a042eb7c17a9b20b0c6e1c1ccb
shubh101295 <shubhagrawal101295@gmail.com>
2021-01-11 22:45:43 +0530

    Add routing within forms

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -20,10 +20,32 @@ export class SignUpComponent implements OnInit {
 		this.password2 = event.target.value;
 	}	
 
+	validateEmail(email){
+		var re = /\S+@\S+\.\S+/;
+        return re.test(email);
+	}
+
+  	changeEmail(event) {
+		console.log(event.target.value);
+		this.email = event.target.value;
+		if(this.validateEmail(event.target.value)===false)
+		{
+			this.emailError = "Email is not valid";
+		}
+		else{
+			this.emailError = "";	
+		}
+		this.emailTouched=true;
+	}
+
+
 	username = ""
 	password1 = ""
 	password2 = ""
-	
+	email = "";
+	emailTouched = false;
+	emailError = "Email should be not empty"
+
 	constructor() { }
 
   ngOnInit() {
